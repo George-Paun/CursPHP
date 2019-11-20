d06d4e4104a45c1240ba600b49d453b245d37150
Adrian Adiaconitei <adrian@hipo.ro>
2019-11-20 23:24:25 +0200

    curs 4

---
+++ curs4/listacarti.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lista carti generata din PHP</title>
+    </head>
+
+    <body>
+        <div class="container clearfix">
+            <nav class="navbar fixed-top navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Lista de carti</a>
+            </nav>
+        </div>
+        <div style="padding-top: 50px;"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-sm-4 col-md-4 col-lg-4 mt-2 ">
+                    <div class="card ">
+                        <img class="card-img-top" src="images/things-fall-apart.jpg" alt="Card image" style="width:100%">
+                        <div class="card-body">
+                            <h4 class="card-title">Things Fall Apart</h4>
+                            <h6 class="card-subtitle mb-2 text-muted">Chinua Achebe, Nigeria</h6>
+                            <p class="card-text">Language : English; Pages:209; Year: 1958</p>
+                            <a href="https://en.wikipedia.org/wiki/Things_Fall_Apart" class="btn btn-primary">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </body>
+
+</html>
--- index.php
+++ index.php
@@ -4,4 +4,15 @@ $x =4;
 $y =6;
 echo $x;
 
+function test (){
+   // $x = func_num_args();
+   
+    // for ($i = 0; $i<$x;$i++){
+    //     echo "parametru $i este: " . func_get_arg($i) . "<br>";
+    // }
+    echo 'test';
+}
+$x =3;
+test($x);
+
 ?>
