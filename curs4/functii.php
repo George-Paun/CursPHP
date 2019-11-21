3ec515b2ab8ea82ec143ef3df0089f354cfe92cb
Adrian Adiaconitei <adrian@hipo.ro>
2019-11-21 19:05:49 +0200

    curs 4 variabile scop

--- curs4/functii.php
+++ curs4/functii.php
@@ -1,7 +1,7 @@
 <?php
-echo "Salut, asa scriu mult si pierd timp! <br>";
-echo "Salut, asa scriu mult si pierd timp! <br>";
-echo "Salut, asa scriu mult si pierd timp! <br>";
+echo "Salut, asa scriu mult si pierd timp! <br>\n";
+echo "Salut, asa scriu mult si pierd timp! <br>\n";
+echo "Salut, asa scriu mult si pierd timp! <br>\n";
 
 // 1. functia salut
 // function salut()
@@ -60,6 +60,37 @@ echo "Salut, asa scriu mult si pierd timp! <br>";
 //  $x = 15;
 //  cubul($x);
 //  echo $x;
+
+// 5.1 Alta functie ce primeste parametru prin referinta
+// function schimbaNume(& $nume)
+// {
+//     $numeTata = "Popescu";
+//     $nume = $numeTata;
+//     echo "Toata familia acum are nume schimbat ", $nume, "<br>\n";
+// }
+
+// $numeMama = "Ionescu";
+// $numeCopil='';
+// schimbaNume($numeMama);
+// schimbaNume($numeCopil);
+// echo $numeMama;
+// echo $numeCopil;
+
+
+//5.2 functie ce returneaza valori prin referinta
+function & schimbaAnotimp($anotimp) {
+//function & schimbaAnotimp( & $anotimp) {
+    $anotimp = 'Primavara';
+    return $anotimp;
+}
+
+$a = 'Iarna';
+echo "\$a este inanite sa schimbaAnotimp(): $a\n";
+//$b = schimbaAnotimp($a); 
+ $b = & schimbaAnotimp($a); 
+echo "\$a este dupa ce am schimbaAnotimp(): $a\n";
+$b = 'Vara';
+echo "\$a dupa ce am modificat valoarea anotimpului b: $a\n";
 
 // 6. functie ce va genera un CNP
 /**
@@ -131,60 +162,60 @@ echo "Salut, asa scriu mult si pierd timp! <br>";
     https://ro.wikipedia.org/wiki/Cod_numeric_personal
     https://github.com/cristian-datu/CNP/blob/master/cnp.php
     */
-    function validCNP($p_cnp)
-    {
-        // CNP must have 13 characters
-        if (strlen($p_cnp) != 13) {
-            return false;
-        }
-        $cnp = str_split($p_cnp);
-        unset( $p_cnp );
-        $hashTable  = array( 2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9 );
-        $hashResult = 0;
-        // All characters must be numeric
-        for ($i = 0; $i < 13; $i++) {
-            if ( ! is_numeric($cnp[$i])) {
-                return false;
-            }
-            $cnp[$i] = (int) $cnp[$i];
-            if ($i < 12) {
-                $hashResult += (int) $cnp[$i] * (int) $hashTable[$i];
-            }
-        }
-        unset( $hashTable, $i );
-        $hashResult = $hashResult % 11;
-        if ($hashResult == 10) {
-            $hashResult = 1;
-        }
-        // Check Year
-        $year = ( $cnp[1] * 10 ) + $cnp[2];
-        switch ($cnp[0]) {
-            case 1  :
-            case 2 :
-                $year += 1900;
-                break; // cetateni romani nascuti intre 1 ian 1900 si 31 dec 1999
-            case 3  :
-            case 4 :
-                $year += 1800;
-                break; // cetateni romani nascuti intre 1 ian 1800 si 31 dec 1899
-            case 5  :
-            case 6 :
-                $year += 2000;
-                break; // cetateni romani nascuti intre 1 ian 2000 si 31 dec 2099
-            case 7  :
-            case 8 :
-            case 9 :                // rezidenti si Cetateni Straini
-                $year += 2000;
-                if ($year > (int) date('Y') - 14) {
-                    $year -= 100;
-            }
-                break;
-            default : {
-                return false;
-            }
-                break;
-        }
-        return ( $year > 1800 && $year < 2099 && $cnp[12] == $hashResult );
-    }
-    echo validCNP('1930101021162');
-    echo validCNP('6070510508288');
+    // function validCNP($p_cnp)
+    // {
+    //     // CNP must have 13 characters
+    //     if (strlen($p_cnp) != 13) {
+    //         return false;
+    //     }
+    //     $cnp = str_split($p_cnp);
+    //     unset( $p_cnp );
+    //     $hashTable  = array( 2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9 );
+    //     $hashResult = 0;
+    //     // All characters must be numeric
+    //     for ($i = 0; $i < 13; $i++) {
+    //         if ( ! is_numeric($cnp[$i])) {
+    //             return false;
+    //         }
+    //         $cnp[$i] = (int) $cnp[$i];
+    //         if ($i < 12) {
+    //             $hashResult += (int) $cnp[$i] * (int) $hashTable[$i];
+    //         }
+    //     }
+    //     unset( $hashTable, $i );
+    //     $hashResult = $hashResult % 11;
+    //     if ($hashResult == 10) {
+    //         $hashResult = 1;
+    //     }
+    //     // Check Year
+    //     $year = ( $cnp[1] * 10 ) + $cnp[2];
+    //     switch ($cnp[0]) {
+    //         case 1  :
+    //         case 2 :
+    //             $year += 1900;
+    //             break; // cetateni romani nascuti intre 1 ian 1900 si 31 dec 1999
+    //         case 3  :
+    //         case 4 :
+    //             $year += 1800;
+    //             break; // cetateni romani nascuti intre 1 ian 1800 si 31 dec 1899
+    //         case 5  :
+    //         case 6 :
+    //             $year += 2000;
+    //             break; // cetateni romani nascuti intre 1 ian 2000 si 31 dec 2099
+    //         case 7  :
+    //         case 8 :
+    //         case 9 :                // rezidenti si Cetateni Straini
+    //             $year += 2000;
+    //             if ($year > (int) date('Y') - 14) {
+    //                 $year -= 100;
+    //         }
+    //             break;
+    //         default : {
+    //             return false;
+    //         }
+    //             break;
+    //     }
+    //     return ( $year > 1800 && $year < 2099 && $cnp[12] == $hashResult );
+    // }
+    // echo validCNP('1930101021162');
+    // echo validCNP('6070510508288');
