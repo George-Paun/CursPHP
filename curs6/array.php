bb4d5ccbd160ae0486babeed9ba7dda60080a35e
aadiaconitei <aadiaconitei@gmail.com>
2020-01-16 13:44:35 +0200

    Update array.php

--- curs6/array.php
+++ curs6/array.php
@@ -77,4 +77,24 @@ $array = array(11, 2, 0,3, 14, 5);
 echo(getMax($array)); 
 echo "<br>"; 
 echo(getMin($array)); 
+
+// Sa se determine mumarul maxim de 3 cifre consecutive dintr-un array
+//$arr =[2,3,7,8,1,3,8,0,9];
+$arr =[1,8,2,3,9,0,0,6,4,2,7,8,2,5];
+//voi genera un nou array cu numere de 3 cifre consecutive
+$n =[];
+$count = count($arr);
+foreach ($arr as $k=>$a){
+    // verific sa nu ajung l final cu numere de 2 sau 1 cifra 
+    // adica elimin 25 si 5 
+    // si numerele care incep cu 0
+    if($k+2<$count && $a !=0){
+        $n[$k] = $arr[$k].$arr[$k+1].$arr[$k+2];
+    }
+    
+}
+print_r($n);
+
+// din acest array returnex maximul
+$max = 0;
 ?> 
